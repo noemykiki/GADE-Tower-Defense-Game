cf6918c586eea1b58f2924d08506ef8723df8d68
OmarAlJanabi <108219949+OmarAlJanabi@users.noreply.github.com>
2024-09-07 22:06:45 +0200

    Map Generation Complete

--- Tower Defense Game/Assets/Scripts - O/Map Generator.cs
+++ Tower Defense Game/Assets/Scripts - O/Map Generator.cs
@@ -7,10 +7,12 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public GameObject MapTile;
+    public Sprite PathTileSprite;
     private GameObject currentTile;
     GameObject startTile;
     GameObject endTile;
-    private bool reachedX = false;
+    private bool reachedX ;
+    private bool reachedY ;  
     private int currentIndex;
     private int nextIndex;
 
@@ -48,20 +50,29 @@ public class MapGenerator : MonoBehaviour
         }
         return edgeTiles;
     }
-
+   
     private void moveDown()
     {
-
+        pathTiles.Add(currentTile);
+        currentIndex = mapTiles.IndexOf(currentTile);
+        nextIndex = currentIndex - mapWidth;
+        currentTile = mapTiles[nextIndex];
     }
 
     private void moveLeft()
     {
-
+        pathTiles.Add(currentTile);
+        currentIndex = mapTiles.IndexOf(currentTile);
+        nextIndex = currentIndex - 1;
+        currentTile = mapTiles[nextIndex];  
     }
 
     private void moveRight()
     {
-
+        pathTiles.Add(currentTile);
+        currentIndex = mapTiles.IndexOf(currentTile);
+        nextIndex = currentIndex + 1;
+        currentTile = mapTiles[nextIndex];
     }
 
     private void generateMap() // generates map
@@ -78,25 +89,76 @@ public class MapGenerator : MonoBehaviour
 
         List<GameObject> topTiles = getTopTiles();     //list of the tiles at the top of the map
         List<GameObject> bottomTiles = getBottomTiles(); // list of tiles at the bottom of the map
-
-        int randTop = UnityEngine.Random.Range(0, mapWidth);  //random number to choose a tile at the top of the map
-        int ranBottom = UnityEngine.Random.Range(0, mapWidth); //random number to choose a tile at the bottom of the map
-
-        startTile = topTiles[randTop];
-        endTile = bottomTiles[ranBottom];
-        currentTile = startTile;
-        while (!reachedX)
+       
+           
+            int ranBottom = UnityEngine.Random.Range(0, mapWidth); //random number to choose a tile at the bottom of the map
+           
+            
+            endTile = bottomTiles[ranBottom];
+        for (int i = 0; i < 3; i++)
         {
-            if (currentTile.transform.position.x > endTile.transform.position.x)
+            reachedX = false;
+            reachedY = false;
+
+     
+            int randTop = UnityEngine.Random.Range(0, mapWidth);  //random number to choose a tile at the top of the map
+            int ranMove = UnityEngine.Random.Range(1, mapHeight - 1);
+
+            startTile = topTiles[randTop];
+
+            currentTile = startTile;
+
+            for (int j = 0; j < ranMove; j++)
             {
-                moveLeft();
+                moveDown();
             }
-            else if (currentTile.transform.position.x < endTile.transform.position.x)
+
+
+            int loop = 0;
+            while (!reachedX)
             {
-                moveRight();
+                loop++;
+                if (loop > 1000)
+                {
+                    break;
+                }
+
+                if (currentTile.transform.position.x > endTile.transform.position.x)
+                {
+                    moveLeft();
+                }
+                else if (currentTile.transform.position.x < endTile.transform.position.x)
+                {
+                    moveRight();
+                }
+                else
+                {
+                    reachedX = true;
+                }
+            }
+
+            while (!reachedY)
+            {
+                if (currentTile.transform.position.y > endTile.transform.position.y)
+                {
+                    moveDown();
+                }
+                else
+                {
+                    reachedY = true;
+                }
+            }
+
+            pathTiles.Add(endTile);
+            foreach (GameObject obj in pathTiles)
+            {
+                SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+                if (spriteRenderer != null)
+                {
+                    spriteRenderer.sprite = PathTileSprite;  // Set the path sprite
+                }
             }
         }
-
     }
 
 }
